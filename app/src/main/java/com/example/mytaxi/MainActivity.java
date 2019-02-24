a9b638a727c353434a7f085d04388f76217968ef
Ruchira <rucsacman@gmail.com>
2019-02-24 15:00:46 +0530

    Customers and Drivers Login completed

--- app/src/main/java/com/example/mytaxi/MainActivity.java
+++ app/src/main/java/com/example/mytaxi/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.mytaxi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mCustomer,mDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDriver=(Button)findViewById(R.id.driver);
+        mCustomer=(Button)findViewById(R.id.customer);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
+
 }
