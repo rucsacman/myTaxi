d037502f547e27b1d2c22ab964d20c24091edf97
Ruchira <rucsacman@gmail.com>
2019-02-25 16:07:47 +0530

    Customer Request Function Implemented.

---
+++ app/src/main/java/com/example/mytaxi/CustomerMapActivity.java
@@ -0,0 +1,200 @@
+package com.example.mytaxi;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CustomerMapActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
+
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mlastLocation;
+    LocationRequest mLocationRequest;
+    private Button mLogout,mRequest;
+    private LatLng pickupLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+
+        mLogout=(Button)findViewById(R.id.logout);
+        mRequest=(Button)findViewById(R.id.request);
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(CustomerMapActivity.this,MainActivity.class);
+                try {
+                    startActivity(intent);
+                    Log.d("Tag_msg", "OnLogOut");
+                    finish();
+                    return;
+                }catch (Exception e){
+                    Log.d("Tag_msg", String.valueOf(e));
+                }
+
+            }
+        });
+
+
+
+        mRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId=FirebaseAuth.getInstance().getCurrentUser().getUid();
+                DatabaseReference ref=FirebaseDatabase.getInstance().getReference("CustomerRequest");
+                GeoFire geoFire=new GeoFire(ref);
+                geoFire.setLocation(userId, new GeoLocation(mlastLocation.getLatitude(), mlastLocation.getLongitude()), new GeoFire.CompletionListener() {
+                    @Override
+                    public void onComplete(String key, DatabaseError error) {
+                        if(error!= null){
+                            Toast.makeText(getApplicationContext(),String.valueOf(error),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                pickupLocation=new LatLng(mlastLocation.getLatitude(),mlastLocation.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(pickupLocation).title("I'm Here"));
+
+                mRequest.setText("Getting Your Driver....");
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d("Tag_msg", "onReady");
+        mMap = googleMap;
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+//                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            Log.d("Tag_msg", "onReady");
+//
+//            return;
+//        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//            return;
+        }
+
+        buildGoogleApiClient();
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.d("Tag_msg", "onBuild");
+        mGoogleApiClient = new GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this).addOnConnectionFailedListener(this).
+                addApi(LocationServices.API).
+                build();
+        mGoogleApiClient.connect();
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mlastLocation = location;
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+        String user_id= FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        Log.d("Tag_msg", String.valueOf(mLocationRequest));
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,
+                this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//    mAuth.addAuthStateListener(firebaseAuthListner);
+        Log.d("Tag_msg", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+}
+
